836b7ed0ae05d896597d72e406365f84b82d81a3
ethanroberson <ethanr.professional@gmail.com>
2024-07-06 19:10:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/services" element={<Services />} />
