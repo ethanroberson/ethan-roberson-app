6fcdc7da150ffe8d9de4c63b5d42f8a2a89316a6
ethanroberson <ethanr.professional@gmail.com>
2024-07-06 13:59:48 -0400

    Update App.js
    Updating App.js home page url path

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/ethan-roberson-app" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/services" element={<Services />} />
